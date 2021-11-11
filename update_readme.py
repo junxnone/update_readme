2ed45742dea295d5bd1a7a1d66aba834b5b03775
JunX <junchen0102@gmail.com>
2021-11-12 02:33:26 +0800

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -16,16 +16,16 @@ for inb in notebook_list:
     file_category[inb] = inb.split('/')[0]
 
 pre_category = 'init'
-with open("README.md.t", 'w') as indexf:
+with open("README.md", 'w') as indexf:
     indexf.write("# Jupyter Notebook Index")
 for ifile in file_category:
     category = file_category[ifile]
 
     if category != pre_category:
-        with open("README.md.t", 'a') as indexf:
+        with open("README.md", 'a') as indexf:
             indexf.write("\n\n## " + category + "\n- [" + ifile.split('/')[1] + "](https://junxnone.github.io/" + repo_name + "/nbv.html?notebook_name=" + ifile + ")")
     else:
-        with open("README.md.t", 'a') as indexf:
+        with open("README.md", 'a') as indexf:
             indexf.write("\n- [" + ifile.split('/')[1] + "](https://junxnone.github.io/" + repo_name + "/nbv.html?notebook_name=" + ifile + ")")
 
     pre_category = file_category[ifile]
